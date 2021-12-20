805a9bf18e92ce1f45f51f51d796a0286cfa4cb8
ChildOfAlara7 <49271455+ChildOfAlara7@users.noreply.github.com>
2021-12-20 18:47:02 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -63,8 +63,6 @@ def start():
 
         for j in range(len(eps_ar)):
             db = DBSCAN(eps=eps_ar[j], min_samples=2).fit(vectors)
-            core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-            core_samples_mask[db.core_sample_indices_] = True
             labels = db.labels_
             n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
             if n_clusters < 49 and n_clusters > n_clusters_opt:
